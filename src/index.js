0bd53f2554a68181aa6762b177e68bebab46fa5f
aleurdo <7777846@gmail.com>
2020-03-18 15:12:51 +0300

    init: task is done

--- src/index.js
+++ src/index.js
@@ -1,12 +1,39 @@
+exports.min = function min(array) {
+    if (Array.isArray(array) == false || array == undefined) return 0;
+    else if (array.length == 0) return 0;
+    else {
+        let res = array[0];
+        for (let i = 0; i < array.length; i++) {
+            if (res > array[i])
+                res = array[i];
 
-exports.min = function min (array) {
-  return 0;
+        }
+        return res;
+    }
+
 }
 
-exports.max = function max (array) {
-  return 0;
+
+exports.max = function max(array) {
+    if (Array.isArray(array) == false || array == undefined) return 0;
+    else if (array.length == 0) return 0;
+    else {
+        let res = array[0];
+        for (let i = 0; i < array.length; i++) {
+            if (res < array[i])
+                res = array[i];
+        }
+        return res;
+    }
 }
 
-exports.avg = function avg (array) {
-  return 0;
+exports.avg = function avg(array) {
+    if (Array.isArray(array) == false || array == undefined) return 0;
+    else if (array.length == 0) return 0;
+    else {
+        let summ = 0;
+        array.forEach(item => summ += item);
+        console.log(summ / array.length);
+        return summ / array.length;
+    }
 }
